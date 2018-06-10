a60420082a2cb455b8c09459d4c0f33c358ad5dc
WangSiyang <610161720@qq.com>
2018-06-10 12:10:35 +0800

    Revert "viewUI"
    This reverts commit 9b236f099f66b4bf712f49701e24548d66ff224a.

--- src/conditioner/MasterUI.java
+++ src/conditioner/MasterUI.java
@@ -54,7 +54,11 @@ public class MasterUI {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
+<<<<<<< HEAD
 				new ViewUI(master);
+=======
+				// TODO Auto-generated method stub
+>>>>>>> parent of 9b236f0... viewUI
 				
 			}
 		});
